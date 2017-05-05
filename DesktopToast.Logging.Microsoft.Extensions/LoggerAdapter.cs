8c14c578f1be3ea4119e1186f5603351e12a9f43
Dmitry Shechtman <damnian@gmail.com>
2017-05-05 19:40:56 +0300

    Add solution

---
+++ DesktopToast.Logging.Microsoft.Extensions/LoggerAdapter.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace DesktopToast.Logging.Microsoft.Extensions
+{
+	/// <summary>
+	/// Desktop Toast Microsoft.Extensions.Logging Adapter
+	/// </summary>
+	public class LoggerAdapter : ILogger
+    {
+		private global::Microsoft.Extensions.Logging.ILogger Logger { get; }
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="LoggerAdapter"/> class.
+		/// </summary>
+		/// <param name="loggerFactory">Logger factory.</param>
+		public LoggerAdapter(ILoggerFactory loggerFactory)
+		{
+			Logger = loggerFactory.CreateLogger<ToastManager>();
+		}
+
+		/// <inheritdoc/>
+		public void Log(LogLevel level, object obj)
+		{
+			Logger.Log(GetLogLevel(level), default(EventId), obj, null, null);
+		}
+
+		/// <inheritdoc/>
+		public void Log(LogLevel level, string str)
+		{
+			Logger.Log(GetLogLevel(level), default(EventId), str, null, null);
+		}
+
+		/// <inheritdoc/>
+		public void Log(LogLevel level, string format, params object[] args)
+		{
+			string str = string.Format(format, args);
+			Logger.Log(GetLogLevel(level), default(EventId), str, null, null);
+		}
+
+		private static global::Microsoft.Extensions.Logging.LogLevel GetLogLevel(LogLevel level)
+		{
+			return (global::Microsoft.Extensions.Logging.LogLevel)level;
+		}
+	}
+}
